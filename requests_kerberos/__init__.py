18f274cf348debb9a367cbd38b61dd73b45680ad
Michael Komitee <komitee@deshaw.com>
2013-02-11 12:24:33 -0500

    Adding basic docs

--- requests_kerberos/__init__.py
+++ requests_kerberos/__init__.py
@@ -1,3 +1,22 @@
+"""
+requests Kerberos/GSSAPI authentication library
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Requests is an HTTP library, written in Python, for human beings. This library
+adds optional Kerberos/GSSAPI authentication support and supports mutual
+authentication. Basic GET usage:
+
+    >>> import requests
+    >>> from requests_kerberos import HTTPKerberosAuth
+    >>> r = requests.get("http://example.org", auth=HTTPKerberosAuth())
+
+... or without mutual authentication:
+
+    >>> r = requests.get("http://example.org", auth=HTTPKerberosAuth(require_mutual_auth=False))
+
+
+The entire `requests.api` should be supported.
+"""
 from .kerberos_ import HTTPKerberosAuth, MutualAuthenticationError
 
 
