1afdfacae5fb508013e30a24effc04aefc64ed36
Michael Komitee <mkomitee@gmail.com>
2013-02-10 21:39:06 -0500

    Using alternate patch syntax.
    There's a belief that using the @patch decorator syntax with the standard
    unittest library may hide tests.
    
    https://github.com/requests/requests-kerberos/issues/1

--- test_requests_kerberos.py
+++ test_requests_kerberos.py
@@ -3,28 +3,33 @@
 
 """Tests for requests_kerberos."""
 
-import unittest
-import mock
-import requests_kerberos
+from mock import Mock, patch
 import requests
+import requests_kerberos
+import unittest
 
 # kerberos.authClientInit() is called with the service name (HTTP@FQDN) and
 # returns 1 and a kerberos context object on success. Returns -1 on failure.
-clientInitComplete = mock.Mock(return_value=(1, "CTX"))
-clientInitError = mock.Mock(return_value=(-1, "CTX"))
+clientInitComplete = Mock(return_value=(1, "CTX"))
+clientInitError = Mock(return_value=(-1, "CTX"))
 
 # kerberos.authGSSClientStep() is called with the kerberos context object
 # returned by authGSSClientInit and the negotiate auth token provided in the
 # http response's www-authenticate header. It returns 0 or 1 on success. 0
 # Indicates that authentication is progressing but not complete.
-clientStepComplete = mock.Mock(return_value=1)
-clientStepContinue = mock.Mock(return_value=0)
-clientStepError = mock.Mock(return_value=-1)
+clientStepComplete = Mock(return_value=1)
+clientStepContinue = Mock(return_value=0)
+clientStepError = Mock(return_value=-1)
 
 # kerberos.authGSSCLientResponse() is called with the kerberos context which
 # was initially returned by authGSSClientInit and had been mutated by a call by
 # authGSSClientStep. It returns a string.
-clientResponse = mock.Mock(return_value="GSSRESPONSE")
+clientResponse = Mock(return_value="GSSRESPONSE")
+
+# Note: we're not using the @mock.patch decorator:
+# > My only word of warning is that in the past, the patch decorator hides
+# > tests when using the standard unittest library.
+# > -- sigmavirus24 in https://github.com/requests/requests-kerberos/issues/1
 
 
 class KerberosTestCase(unittest.TestCase):
@@ -53,48 +58,54 @@ class KerberosTestCase(unittest.TestCase):
             None
         )
 
-    @mock.patch('kerberos.authGSSClientResponse', clientResponse)
-    @mock.patch('kerberos.authGSSClientStep', clientStepContinue)
-    @mock.patch('kerberos.authGSSClientInit', clientInitComplete)
     def test_generate_request_header(self):
-        response = requests.Response()
-        response.url = "http://www.example.org/"
-        response.headers = {'www-authenticate': 'negotiate token'}
-        auth = requests_kerberos.HTTPKerberosAuth()
-        self.assertEqual(
-            auth.generate_request_header(response),
-            "Negotiate GSSRESPONSE"
-        )
-        clientInitComplete.assert_called_with("HTTP@www.example.org")
-        clientStepContinue.assert_called_with("CTX", "token")
-        clientResponse.assert_called_with("CTX")
+        with patch.multiple('kerberos',
+                            authGSSClientInit=clientInitComplete,
+                            authGSSClientResponse=clientResponse,
+                            authGSSClientStep=clientStepContinue):
+            response = requests.Response()
+            response.url = "http://www.example.org/"
+            response.headers = {'www-authenticate': 'negotiate token'}
+            auth = requests_kerberos.HTTPKerberosAuth()
+            self.assertEqual(
+                auth.generate_request_header(response),
+                "Negotiate GSSRESPONSE"
+            )
+            clientInitComplete.assert_called_with("HTTP@www.example.org")
+            clientStepContinue.assert_called_with("CTX", "token")
+            clientResponse.assert_called_with("CTX")
 
-    @mock.patch('kerberos.authGSSClientResponse', clientResponse)
-    @mock.patch('kerberos.authGSSClientStep', clientStepContinue)
-    @mock.patch('kerberos.authGSSClientInit', clientInitError)
     def test_generate_request_header_init_error(self):
-        response = requests.Response()
-        response.url = "http://www.example.org/"
-        response.headers = {'www-authenticate': 'negotiate token'}
-        auth = requests_kerberos.HTTPKerberosAuth()
-        self.assertRaises(Exception, auth.generate_request_header, response)
-        clientInitError.assert_called_with("HTTP@www.example.org")
-        clientStepContinue.assert_not_called()
-        clientResponse.assert_not_called()
+        with patch.multiple('kerberos',
+                            authGSSClientInit=clientInitError,
+                            authGSSClientResponse=clientResponse,
+                            authGSSClientStep=clientStepContinue):
+            response = requests.Response()
+            response.url = "http://www.example.org/"
+            response.headers = {'www-authenticate': 'negotiate token'}
+            auth = requests_kerberos.HTTPKerberosAuth()
+            self.assertRaises(Exception,
+                              auth.generate_request_header,
+                              response)
+            clientInitError.assert_called_with("HTTP@www.example.org")
+            clientStepContinue.assert_not_called()
+            clientResponse.assert_not_called()
 
-    @mock.patch('kerberos.authGSSClientResponse', clientResponse)
-    @mock.patch('kerberos.authGSSClientStep', clientStepError)
-    @mock.patch('kerberos.authGSSClientInit', clientInitComplete)
     def test_generate_request_header_step_error(self):
-        response = requests.Response()
-        response.url = "http://www.example.org/"
-        response.headers = {'www-authenticate': 'negotiate token'}
-        auth = requests_kerberos.HTTPKerberosAuth()
-        self.assertRaises(Exception, auth.generate_request_header, response)
-        clientInitComplete.assert_called_with("HTTP@www.example.org")
-        clientStepError.assert_called_with("CTX", "token")
-        clientResponse.assert_not_called()
-
+        with patch.multiple('kerberos',
+                            authGSSClientInit=clientInitComplete,
+                            authGSSClientResponse=clientResponse,
+                            authGSSClientStep=clientStepError):
+            response = requests.Response()
+            response.url = "http://www.example.org/"
+            response.headers = {'www-authenticate': 'negotiate token'}
+            auth = requests_kerberos.HTTPKerberosAuth()
+            self.assertRaises(Exception,
+                              auth.generate_request_header,
+                              response)
+            clientInitComplete.assert_called_with("HTTP@www.example.org")
+            clientStepError.assert_called_with("CTX", "token")
+            clientResponse.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
